e4b11569f002a15ab66a22d6578ef0d7c1623576
JEFFIN SAM GEORGE <jeffinsamrock@gmail.com>
2023-07-25 11:08:40 +0530

    Second commit

--- month/src/index.js
+++ month/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbr from './components/navbr';
+import DemoUpload from './components/demofile';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbr/>
+    <DemoUpload/>
   </React.StrictMode>
 );
 
